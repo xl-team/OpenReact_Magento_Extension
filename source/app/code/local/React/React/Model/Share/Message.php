63d44fd1fe19da653034ce87943577db55273385
Vlad Dragos <vlad@xlteam.nl>
2012-07-17 19:19:52 +0300

    Email form fix and chars limit. 	modified:   source/app/code/local/React/React/Model/Share/Message.php 	modified:   source/app/design/frontend/default/default/template/react/customer/email.phtml

--- source/app/code/local/React/React/Model/Share/Message.php
+++ source/app/code/local/React/React/Model/Share/Message.php
@@ -35,7 +35,30 @@ class React_React_Model_Share_Message extends Varien_Object
 			$data['application_user_id'] = $_helper->encodeApplicationUserId($_helper->getCustomer());
 		if (empty($data['providers']))
 			$data['providers'] = array_values($_helper->getConnectedAccounts($_helper->getCustomer()));
-		$this->addData($data);
+		foreach ($data as $data_key => $data_value)
+		{
+			$key = str_replace(' ', '', ucwords(str_replace('_', ' ', $data_key)));
+			$function = 'set'.$key;
+			$this->$function($data_value);
+		}
+		
 		return $this;
+	}
+	
+	public function setDescription($description)
+	{
+		parent::setDescription($this->_trim($description));
+	}
+
+	public function setMessage($msg)
+	{
+		parent::setMessage($this->_trim($msg));
+	}
+	
+	protected function _trim($string)
+	{
+		if(is_string($string) && strlen($string) > 255)
+			$string = substr($string, 0, 252).'...';
+		return $string;
 	}
 }
